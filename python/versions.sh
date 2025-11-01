50bc45003897c646c17a519ebb66cbc2cbc9cba5
吴小白 <296015668@qq.com>
2025-11-01 14:50:58 +0800

    feat: Remove all traces of the now-EOL 3.9 release series

--- python/versions.sh
+++ python/versions.sh
@@ -162,7 +162,7 @@ for version in "${versions[@]}"; do
 	# TODO remove setuptools version handling entirely once Python 3.11 is EOL
 	setuptoolsVersion="$(sed -nre 's/^_SETUPTOOLS_VERSION[[:space:]]*=[[:space:]]*"(.*?)".*/\1/p' <<<"$ensurepipVersions")"
 	case "$rcVersion" in
-		3.9 | 3.10 | 3.11)
+		3.10 | 3.11)
 			if [ -z "$setuptoolsVersion" ]; then
 				echo >&2 "error: $version: missing setuptools version"
 				exit 1
@@ -170,11 +170,6 @@ for version in "${versions[@]}"; do
 			if ! wget -q -O /dev/null -o /dev/null --spider "https://pypi.org/pypi/setuptools/$setuptoolsVersion/json"; then
 				echo >&2 "error: $version: setuptools version ($setuptoolsVersion) seems to be invalid?"
 				exit 1
-			fi
-
-			# https://github.com/docker-library/python/issues/781 (TODO remove this if 3.10 and 3.11 embed a newer setuptools and this section no longer applies)
-			if [ "$setuptoolsVersion" = '65.5.0' ]; then
-				setuptoolsVersion='65.5.1'
 			fi
 			;;
 
