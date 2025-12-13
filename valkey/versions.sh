37030515e8b81e3a19e69228d167469d0873cfde
吴小白 <296015668@qq.com>
2025-12-13 09:02:20 +0800

    build(valkey): 添加 ci 任务

---
+++ valkey/versions.sh
@@ -0,0 +1,93 @@
+#!/usr/bin/env bash
+set -Eeuo pipefail
+
+alpine="$(
+	bashbrew cat --format '{{ .TagEntry.Tags | join "\n" }}' https://github.com/docker-library/official-images/raw/HEAD/library/alpine:latest \
+		| grep -E '^[0-9]+[.][0-9]+$'
+)"
+[ "$(wc -l <<<"$alpine")" = 1 ]
+export alpine
+
+debian="$(
+	bashbrew cat --format '{{ .TagEntry.Tags | join "\n" }}' https://github.com/docker-library/official-images/raw/HEAD/library/debian:latest \
+		| grep -vE '^latest$|[0-9.-]' \
+		| head -1
+)"
+[ "$(wc -l <<<"$debian")" = 1 ]
+export debian
+
+cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
+
+versions=( "$@" )
+if [ ${#versions[@]} -eq 0 ]; then
+	versions=( */ )
+	json='{}'
+else
+	json="$(< versions.json)"
+fi
+versions=( "${versions[@]%/}" )
+
+packages="$(
+	wget -qO- 'https://github.com/valkey-io/valkey-hashes/raw/main/README' \
+	| jq -csR '
+		rtrimstr("\n")
+		| split("\n")
+		| map(
+			# this capture will naturally ignore comments and blank lines
+			capture(
+				[
+					"^hash[[:space:]]+",
+					"(?<file>valkey-",
+						"(?<version>([0-9.]+)(-rc[0-9]+)?|unstable)",
+					"[.][^[:space:]]+)[[:space:]]+",
+					"(?<type>sha256|sha1)[[:space:]]+", # this filters us down to just the checksum types we are prepared to handle right now
+					"(?<sum>[0-9a-f]{64}|[0-9a-f]{40})[[:space:]]+",
+					"(?<url>[^[:space:]]+)",
+					"$"
+				] | join("")
+			)
+			| {
+				version: .version,
+				url: .url,
+				(.type): .sum,
+			}
+		)
+	'
+)"
+
+for version in "${versions[@]}"; do
+	export version rcVersion="${version%-rc}"
+	if [ "$version" = "unstable" ]; then
+		doc="$(
+			jq <<<"$packages" -c '
+				map(select(.version == "unstable"))[-1]
+			'
+		)"
+	else
+		doc="$(
+			jq <<<"$packages" -c '
+				map(
+					select(
+						.version
+						| (
+							startswith(env.rcVersion + ".")
+							or startswith(env.rcVersion + "-")
+						)
+					)
+				)[-1]
+			'
+		)"
+	fi
+
+	fullVersion="$(jq <<<"$doc" -r '.version')"
+	echo "$version: $fullVersion"
+
+	json="$(jq <<<"$json" -c --argjson doc "$doc" '
+		.[env.version] = ($doc + {
+			debian: { version: env.debian },
+			alpine: { version: env.alpine },
+		})
+	')"
+done
+
+jq <<<"$json" . > versions.json
