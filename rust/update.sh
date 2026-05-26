e5b33b65cb7e7d86aad2e106a9a15ed06605ee80
吴小白 <296015668@qq.com>
2026-05-26 16:33:19 +0800

    perf: 更新任务模板

--- rust/update.sh
+++ rust/update.sh
@@ -2,6 +2,5 @@
 set -Eeuo pipefail
 
 cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
-curl -sSL -O https://github.com/rust-lang/docker-rust/raw/refs/heads/master/versions.toml
 
 python3 x.py update
